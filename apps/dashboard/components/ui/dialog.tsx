4f1dd364aa6b5f5028a4312fa5f045acde836424
iza <59828082+izadoesdev@users.noreply.github.com>
2025-11-30 17:45:59 +0200

    fix: website card X dialog

--- apps/dashboard/components/ui/dialog.tsx
+++ apps/dashboard/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { XIcon } from 'lucide-react';
+import { XIcon } from '@phosphor-icons/react';
 import { Dialog as DialogPrimitive } from 'radix-ui';
 import type * as React from 'react';
 
@@ -68,10 +68,10 @@ function DialogContent({
 				{children}
 				{showCloseButton && (
 					<DialogPrimitive.Close
-						className="absolute top-4 right-4 rounded-xs opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0"
+						className="absolute hover:cursor-pointer top-4 right-4 rounded-xs opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0"
 						data-slot="dialog-close"
 					>
-						<XIcon />
+						<XIcon/>
 						<span className="sr-only">Close</span>
 					</DialogPrimitive.Close>
 				)}
